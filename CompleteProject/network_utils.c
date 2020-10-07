463a6e411483f9c55518c7b3a56f7f848645449c
Abhishek Sagar <sachinites@gmail.com>
2020-10-07 14:29:51 -0700

    TCP DB mgmt in progress

--- CompleteProject/network_utils.c
+++ CompleteProject/network_utils.c
@@ -16,25 +16,47 @@
  * =====================================================================================
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <memory.h>
-#include <netdb.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <assert.h>
 #include "network_utils.h"
+
+#define TCP_DB_MGMT
+
+#ifdef TCP_DB_MGMT
+static pthread_mutex_t tcp_db_mgmt_mutex;
+#endif
+
+/* 
+ * Library holds the pointer to application instantiated
+ * tcp_connections_db 
+ * */
+static tcp_connections_db_t *tcp_connections_db = NULL;
+
+void
+init_network_skt_lib(tcp_connections_db_t 
+		*_tcp_connections_db){
+
+	static bool initialized = false;
+	
+	if(initialized) return;
+
+	initialized = true;
+
+	tcp_connections_db = _tcp_connections_db;
+
+	init_glthread(&tcp_connections_db->tcp_server_list_head);
+
+	#ifdef TCP_DB_MGMT
+	pthread_mutex_init(&tcp_db_mgmt_mutex, NULL);
+	#endif
+}
+
 
 typedef struct thread_arg_pkg_{
 
 	char ip_addr[16];
 	uint32_t port_no;
 	recv_fn_cb recv_fn;
-	tcp_conn_init_req_cb conn_init_req_fn;
-	tcp_conn_killed_cb conn_killed_fn;
+	tcp_connect_cb tcp_connect_fn;
+	tcp_disconnect_cb tcp_disconnect_fn;
 } thread_arg_pkg_t;
 
 
@@ -52,13 +74,13 @@ _network_start_udp_pkt_receiver_thread(void *arg){
 	
 	/* Not applicable for UDP communication as in
  	 * UDP communication there is really no Connection Establishment */
-	tcp_conn_init_req_cb conn_init_req_fn = thread_arg_pkg->conn_init_req_fn;
-	assert(!conn_init_req_fn);
+	tcp_connect_cb tcp_connect_fn = thread_arg_pkg->tcp_connect_fn;
+	assert(!tcp_connect_fn);
 
 	/* When there is no connection establishment, then there can be no
  	 * connection abrupt termination (ctrl-C on client) */
-	tcp_conn_killed_cb tcp_conn_killed_fn = thread_arg_pkg->conn_killed_fn;
-	assert(!tcp_conn_killed_fn);
+	tcp_disconnect_cb tcp_tcp_disconnect_fn = thread_arg_pkg->tcp_disconnect_fn;
+	assert(!tcp_tcp_disconnect_fn);
 	
 	free(thread_arg_pkg);
 	thread_arg_pkg = NULL;
@@ -130,8 +152,8 @@ network_start_udp_pkt_receiver_thread(
 	strncpy(thread_arg_pkg->ip_addr, ip_addr, 16);
 	thread_arg_pkg->port_no = udp_port_no;
 	thread_arg_pkg->recv_fn = recv_fn;
-	thread_arg_pkg->conn_init_req_fn = NULL;
-	thread_arg_pkg->conn_killed_fn = NULL;
+	thread_arg_pkg->tcp_connect_fn = NULL;
+	thread_arg_pkg->tcp_disconnect_fn = NULL;
 
     pthread_create(&recv_pkt_thread, &attr,
 			_network_start_udp_pkt_receiver_thread,
@@ -194,7 +216,7 @@ add_to_monitored_tcp_fd_set(int skt_fd,
     }
 }
 
-static void
+static int
 remove_from_monitored_tcp_fd_set(int skt_fd,
 								 int fd_arr[],
 								 int fd_arr_size){
@@ -207,8 +229,9 @@ remove_from_monitored_tcp_fd_set(int skt_fd,
             continue;
 
         fd_arr[i] = -1;
-        break;
+		return i;
     }
+	return -1;
 }
 
 static int
@@ -257,23 +280,40 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
 	char *ip_addr 	   = thread_arg_pkg->ip_addr;
 	uint32_t port_no   = thread_arg_pkg->port_no;
 	recv_fn_cb recv_fn = thread_arg_pkg->recv_fn;
-	tcp_conn_init_req_cb conn_init_req_fn = thread_arg_pkg->conn_init_req_fn;
-    tcp_conn_killed_cb conn_killed_fn = thread_arg_pkg->conn_killed_fn;
+	tcp_connect_cb tcp_connect_fn = thread_arg_pkg->tcp_connect_fn;
+    tcp_disconnect_cb tcp_disconnect_fn = thread_arg_pkg->tcp_disconnect_fn;
 	
 	free(thread_arg_pkg);
 	thread_arg_pkg = NULL;
 
-	monitored_tcp_fd_set_array = calloc(MAX_CLIENT_TCP_CONNECTION_SUPPORTED,
-								 sizeof(int));
+	#ifdef TCP_DB_MGMT
+		tcp_server_t *tcp_server = calloc(1, sizeof(tcp_server_t));
+		strncpy(tcp_server->ip_addr, ip_addr, 16);
+		tcp_server->port_no = port_no;
+		tcp_server->recv_fn = recv_fn;
+		tcp_server->tcp_disconnect_fn = tcp_disconnect_fn;
+		tcp_server->tcp_connect_fn = tcp_connect_fn;
+	    monitored_tcp_fd_set_array = tcp_server->monitored_tcp_fd_set_array;
+		init_glthread(&tcp_server->clients_list_head);
+		init_glthread(&tcp_server->glue);
+		pthread_mutex_lock(&tcp_db_mgmt_mutex);
+		tcp_server_add_to_db(tcp_connections_db, tcp_server);
+		pthread_mutex_unlock(&tcp_db_mgmt_mutex);
+	#else
+		monitored_tcp_fd_set_array = calloc(MAX_CLIENT_TCP_CONNECTION_SUPPORTED,
+											 sizeof(int));	
+	#endif
 
 	intitialize_monitored_fd_set(monitored_tcp_fd_set_array,
 								 MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
 	
 	int tcp_master_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP );
+	int tcp_dummy_master_sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP );
 
-	if(tcp_master_sock_fd == -1){
+	if(tcp_master_sock_fd == -1 || 
+		tcp_dummy_master_sock_fd == -1){
 		printf("Socket Creation Failed\n");
-		return 0;
+		goto CLEANUP;
 	}
 
 	struct sockaddr_in server_addr;
@@ -284,30 +324,65 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
     if (setsockopt(tcp_master_sock_fd, SOL_SOCKET, 
 				   SO_REUSEADDR, (char *)&opt, sizeof(opt))<0) {
 		printf("setsockopt Failed\n");
-		return 0;
+		goto CLEANUP;
     }
     
 	if (setsockopt(tcp_master_sock_fd, SOL_SOCKET,
 				   SO_REUSEPORT, (char *)&opt, sizeof(opt))<0) {
 		printf("setsockopt Failed\n");
-		return 0;
+		goto CLEANUP;
     }
 
 	if (bind(tcp_master_sock_fd, (struct sockaddr *)&server_addr,
 				sizeof(struct sockaddr)) == -1) {
 		printf("Error : socket bind failed\n");
-		return 0;
+		goto CLEANUP;
 	}
 
 	if (listen(tcp_master_sock_fd, 5) < 0 ) {
 		
 		printf("listen failed\n");
-		return 0;
+		goto CLEANUP;
 	}
 			
+	server_addr.sin_port        = port_no + 1;
+
+    if (setsockopt(tcp_dummy_master_sock_fd, SOL_SOCKET, 
+				   SO_REUSEADDR, (char *)&opt, sizeof(opt))<0) {
+		printf("setsockopt Failed\n");
+		goto CLEANUP;
+    }
+    
+	if (setsockopt(tcp_dummy_master_sock_fd, SOL_SOCKET,
+				   SO_REUSEPORT, (char *)&opt, sizeof(opt))<0) {
+		printf("setsockopt Failed\n");
+		goto CLEANUP;
+    }
+
+	if (bind(tcp_dummy_master_sock_fd, (struct sockaddr *)&server_addr,
+				sizeof(struct sockaddr)) == -1) {
+		printf("Error : socket bind failed\n");
+		goto CLEANUP;
+	}
+
+	if (listen(tcp_dummy_master_sock_fd, 5) < 0 ) {
+		
+		printf("listen failed\n");
+		goto CLEANUP;
+	}
+	
 	add_to_monitored_tcp_fd_set(tcp_master_sock_fd,
 								monitored_tcp_fd_set_array,
 								MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
+	
+	add_to_monitored_tcp_fd_set(tcp_dummy_master_sock_fd,
+								monitored_tcp_fd_set_array,
+								MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
+
+	#ifdef TCP_DB_MGMT
+		tcp_server->master_sock_fd = tcp_master_sock_fd;
+		tcp_server->dummy_master_sock_fd = tcp_dummy_master_sock_fd;
+	#endif
 
 	char *recv_buffer = calloc(1, MAX_PACKET_BUFFER_SIZE);
 
@@ -318,6 +393,7 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
     FD_ZERO(&backup_sock_fd_set);
 
     FD_SET(tcp_master_sock_fd, &backup_sock_fd_set);
+    FD_SET(tcp_dummy_master_sock_fd, &backup_sock_fd_set);
 
     struct sockaddr_in client_addr;
     int bytes_recvd = 0,
@@ -349,15 +425,40 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
 						  monitored_tcp_fd_set_array,
 						  MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
 
-			if(conn_init_req_fn) conn_init_req_fn( 0, 0);
+			#ifdef TCP_DB_MGMT
+
+			tcp_connected_client_t *tcp_connected_client = 
+					calloc(1, sizeof(tcp_connected_client_t));
+
+			tcp_create_new_tcp_connection_client_entry(
+					comm_socket_fd,
+					network_covert_ip_n_to_p((uint32_t)client_addr.sin_addr.s_addr, 0), 
+					client_addr.sin_port, tcp_connected_client);
+			
+			pthread_mutex_lock(&tcp_db_mgmt_mutex);
+			tcp_save_tcp_server_client_entry(
+				tcp_connections_db,
+				tcp_master_sock_fd,
+				tcp_connected_client);
+			pthread_mutex_unlock(&tcp_db_mgmt_mutex);
+
+			#endif
+
+			if(tcp_connect_fn) tcp_connect_fn( 0, 0);
         }
+		else if(FD_ISSET(tcp_dummy_master_sock_fd, &active_sock_fd_set)){
+			
+			fd_set_skt_fd(&backup_sock_fd_set,
+						  monitored_tcp_fd_set_array,
+						  MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
+		}
 		else {
 			int i = 0;
 			for ( i = 0; i < MAX_CLIENT_TCP_CONNECTION_SUPPORTED; i++){
 				
 				if(monitored_tcp_fd_set_array[i] == -1 || 
 				   monitored_tcp_fd_set_array[i] == tcp_master_sock_fd) {
-						continue;
+					continue;
 				}
 
 				if(FD_ISSET(monitored_tcp_fd_set_array[i], &active_sock_fd_set)){
@@ -383,7 +484,18 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
 						  monitored_tcp_fd_set_array,
 						  MAX_CLIENT_TCP_CONNECTION_SUPPORTED);
 
-						if(conn_killed_fn) conn_killed_fn(0, 0);
+						if(tcp_disconnect_fn) tcp_disconnect_fn(0, 0);
+						
+						#ifdef TCP_DB_MGMT
+							pthread_mutex_lock(&tcp_db_mgmt_mutex);
+							tcp_connected_client_t *tcp_connected_client = 
+								tcp_lookup_tcp_server_client_entry_by_comm_fd(
+									tcp_connections_db, comm_socket_fd);
+							assert(tcp_connected_client);
+							tcp_delete_tcp_server_client_entry(tcp_connected_client);		
+							pthread_mutex_unlock(&tcp_db_mgmt_mutex);
+						#endif
+
 					} 
 					else {
 						recv_fn(recv_buffer, bytes_recvd, 0, 0, 0, 0);					
@@ -392,6 +504,13 @@ _network_start_tcp_pkt_receiver_thread(void *arg){
 			}
 		}
     }
+	CLEANUP:
+	#ifdef TCP_DB_MGMT
+	 	pthread_mutex_lock(&tcp_db_mgmt_mutex);
+		tcp_remove_tcp_server_entry(tcp_server);
+		pthread_mutex_unlock(&tcp_db_mgmt_mutex);
+		free(tcp_server);	
+	#endif
     return 0;
 }
 
@@ -401,8 +520,8 @@ network_start_tcp_pkt_receiver_thread(
         char *ip_addr,
         uint32_t tcp_port_no,
 		recv_fn_cb recv_fn,
-		tcp_conn_init_req_cb conn_init_req_fn,
-	    tcp_conn_killed_cb conn_killed_fn) {
+		tcp_connect_cb tcp_connect_fn,
+	    tcp_disconnect_cb tcp_disconnect_fn) {
 
     pthread_attr_t attr;
     pthread_t recv_pkt_thread;
@@ -415,8 +534,8 @@ network_start_tcp_pkt_receiver_thread(
 	strncpy(thread_arg_pkg->ip_addr, ip_addr, 16);
 	thread_arg_pkg->port_no = tcp_port_no;
 	thread_arg_pkg->recv_fn = recv_fn;
-	thread_arg_pkg->conn_init_req_fn = conn_init_req_fn;
-	thread_arg_pkg->conn_killed_fn = conn_killed_fn;
+	thread_arg_pkg->tcp_connect_fn = tcp_connect_fn;
+	thread_arg_pkg->tcp_disconnect_fn = tcp_disconnect_fn;
 
     pthread_create(&recv_pkt_thread, &attr,
 			_network_start_tcp_pkt_receiver_thread,
@@ -425,7 +544,7 @@ network_start_tcp_pkt_receiver_thread(
 
 
 int
-send_tcp_msg(int tcp_comm_fd,
+tcp_send_msg(int tcp_comm_fd,
              char *msg,
              uint32_t msg_size) {
 
@@ -463,4 +582,252 @@ tcp_connect(char *tcp_server_ip,
 	}
 	
 	return sock_fd;
+}
+
+void
+tcp_fake_connect(char *tcp_server_ip,
+	  			 uint32_t tcp_server_port_no){
+
+	int rc = 0;
+	int sock_fd = -1;
+
+	if ((sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1){
+		return;
+	}
+
+	struct hostent *host;
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+
+	host = (struct hostent *)gethostbyname(tcp_server_ip);
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons((unsigned short)tcp_server_port_no);
+	server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+
+	rc = connect(sock_fd, (struct sockaddr *)&server_addr,sizeof(struct sockaddr));
+
+	close(sock_fd);	
+}
+
+void
+tcp_disconnect(int comm_sock_fd,
+               char *good_bye_msg,
+               uint32_t good_bye_msg_size) {
+
+	if(good_bye_msg && good_bye_msg_size > 0 ) {
+		tcp_send_msg(comm_sock_fd, good_bye_msg, good_bye_msg_size);
+	}
+
+	close(comm_sock_fd);
+}
+
+
+#ifdef TCP_DB_MGMT
+
+tcp_connected_client_t *
+tcp_lookup_tcp_server_client_entry_by_comm_fd(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t comm_fd){
+
+	glthread_t *curr, *curr2;
+	tcp_server_t *tcp_server_curr;
+	tcp_connected_client_t *tcp_connected_client_curr;
+
+	ITERATE_GLTHREAD_BEGIN(&tcp_connections_db->tcp_server_list_head, curr){
+
+		tcp_server_curr = glue_to_tcp_server(curr);
+
+		ITERATE_GLTHREAD_BEGIN(&tcp_server_curr->clients_list_head, curr2){
+
+			tcp_connected_client_curr = glue_to_tcp_connected_client(curr2);
+
+			if(tcp_connected_client_curr->client_comm_fd == comm_fd) {
+				
+				return tcp_connected_client_curr;
+			}
+		}ITERATE_GLTHREAD_END(&tcp_server_curr->clients_list_head, curr2);				
+
+	} ITERATE_GLTHREAD_END(&tcp_connections_db->tcp_server_list_head, curr);
+	return NULL;
+}
+
+void
+tcp_delete_tcp_server_client_entry(
+	tcp_connected_client_t *tcp_connected_client){
+
+	remove_glthread(&tcp_connected_client->glue);
+	tcp_connected_client->tcp_server = NULL;
+	free(tcp_connected_client);
+}
+
+
+void
+tcp_force_disconnect_client_by_comm_fd(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t comm_fd){
+
+	tcp_connected_client_t * tcp_connected_client = 
+		tcp_lookup_tcp_server_client_entry_by_comm_fd(
+			tcp_connections_db, comm_fd);
+
+	if ( !tcp_connected_client) return;
+
+	tcp_server_t *tcp_server = tcp_connected_client->tcp_server;
+	
+	assert(tcp_server);
+
+	assert((remove_from_monitored_tcp_fd_set(
+			tcp_connected_client->client_comm_fd,
+			tcp_server->monitored_tcp_fd_set_array,
+			sizeof(tcp_server->monitored_tcp_fd_set_array)/\
+				sizeof(tcp_server->monitored_tcp_fd_set_array[0]))>= 0));
+
+	tcp_fake_connect(tcp_server->ip_addr, tcp_server->port_no + 1);
+	
+	tcp_delete_tcp_server_client_entry(tcp_connected_client);
+}
+
+void
+tcp_create_new_tcp_connection_client_entry(
+    int client_comm_fd,
+    char *client_ip_addr,
+    uint32_t client_tcp_port_no,
+    tcp_connected_client_t *tcp_connected_client/*  o/p */) {
+
+	tcp_connected_client->client_comm_fd = client_comm_fd;
+	strncpy(tcp_connected_client->client_ip_addr,
+			client_ip_addr, 16);
+	tcp_connected_client->client_tcp_port_no = client_tcp_port_no;
+	tcp_connected_client->tcp_server = NULL;
+	init_glthread(&tcp_connected_client->glue);
+}
+
+void
+tcp_save_tcp_server_client_entry(
+    tcp_connections_db_t *tcp_connections_db,
+    uint32_t tcp_server_master_sock_fd,
+    tcp_connected_client_t *tcp_connected_client){
+
+	tcp_server_t *tcp_server = tcp_lookup_tcp_server_entry_by_master_fd(
+								tcp_connections_db,
+						        tcp_server_master_sock_fd);
+
+	assert(tcp_server);
+
+	assert(!tcp_connected_client->tcp_server);
+
+	tcp_connected_client->tcp_server = tcp_server;
+
+	init_glthread(&tcp_connected_client->glue);
+
+	glthread_add_next(&tcp_server->clients_list_head, &tcp_connected_client->glue);
+}
+
+tcp_server_t *
+tcp_lookup_tcp_server_entry_by_master_fd(
+    tcp_connections_db_t *tcp_connections_db,
+    uint32_t master_fd){
+
+	glthread_t *curr;
+	tcp_server_t *tcp_server_curr;
+
+	ITERATE_GLTHREAD_BEGIN(&tcp_connections_db->tcp_server_list_head, curr){
+
+		tcp_server_curr = glue_to_tcp_server(curr);
+
+		if(tcp_server_curr->master_sock_fd == master_fd)
+			return tcp_server_curr;
+
+	} ITERATE_GLTHREAD_END(&tcp_connections_db->tcp_server_list_head, curr);
+	return NULL;
+}
+
+static void
+print_tcp_connected_client(
+		tcp_connected_client_t *tcp_connected_client){
+
+	printf("\tClient : [%s %u]\n", tcp_connected_client->client_ip_addr,
+								   tcp_connected_client->client_tcp_port_no);
+}
+
+static void
+print_tcp_server_info(tcp_server_t *tcp_server){
+
+	glthread_t *curr;
+	tcp_connected_client_t *tcp_connected_client;
+
+	printf("TCP Server : [%s %u]\n", tcp_server->ip_addr, tcp_server->port_no);
+
+	ITERATE_GLTHREAD_BEGIN(&tcp_server->clients_list_head, curr){
+
+		tcp_connected_client = glue_to_tcp_connected_client(curr);
+		print_tcp_connected_client(tcp_connected_client);
+
+	} ITERATE_GLTHREAD_END(&tcp_server->clients_list_head, curr);	
+}
+
+void
+tcp_dump_tcp_connection_db(
+        tcp_connections_db_t *tcp_connections_db){
+
+
+	glthread_t *curr;
+	tcp_server_t *tcp_server_curr;
+
+	ITERATE_GLTHREAD_BEGIN(&tcp_connections_db->tcp_server_list_head, curr){
+
+		tcp_server_curr = glue_to_tcp_server(curr);
+
+		print_tcp_server_info(tcp_server_curr);
+
+	} ITERATE_GLTHREAD_END(&tcp_connections_db->tcp_server_list_head, curr);
+}
+
+void
+tcp_remove_tcp_server_entry(
+    tcp_server_t *tcp_server){
+
+	assert(IS_GLTHREAD_LIST_EMPTY(&tcp_server->clients_list_head));	
+	remove_glthread(&tcp_server->glue);
+}
+
+void
+tcp_server_add_to_db(
+    tcp_connections_db_t *tcp_connections_db,
+    tcp_server_t *tcp_server) {
+
+	assert(IS_GLTHREAD_LIST_EMPTY(&tcp_server->glue));
+
+	assert(!tcp_lookup_tcp_server_entry_by_master_fd(
+				tcp_connections_db, tcp_server->master_sock_fd));
+
+	glthread_add_next(&tcp_connections_db->tcp_server_list_head,
+					  &tcp_server->glue);
+}
+
+
+#endif
+
+char *
+network_covert_ip_n_to_p(uint32_t ip_addr,
+                    char *output_buffer){
+
+    char *out = NULL;
+    static char str_ip[16];
+    out = !output_buffer ? str_ip : output_buffer;
+    memset(out, 0, 16);
+    ip_addr = htonl(ip_addr);
+    inet_ntop(AF_INET, &ip_addr, out, 16);
+    out[15] = '\0';
+    return out;
+}
+
+uint32_t
+network_covert_ip_p_to_n(char *ip_addr){
+
+    uint32_t binary_prefix = 0;
+    inet_pton(AF_INET, ip_addr, &binary_prefix);
+    binary_prefix = htonl(binary_prefix);
+    return binary_prefix;
 }
--- CompleteProject/network_utils.h
+++ CompleteProject/network_utils.h
@@ -20,6 +20,19 @@
 #define __NETWORK_UTILS__
 
 #include <stdint.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <memory.h>
+#include <netdb.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <assert.h>
+#include "gluethread/glthread.h"
 
 #define MAX_PACKET_BUFFER_SIZE				1024
 #define MAX_CLIENT_TCP_CONNECTION_SUPPORTED	256
@@ -32,11 +45,130 @@ typedef void (*recv_fn_cb)(char *,		/* msg recvd */
 						   uint32_t,	/* Sender Communication FD , only for tcp*/
 						   int []);		/* Monitored FD set array  , only for tcp*/
 
-typedef void (*tcp_conn_init_req_cb)(char *,     /* Client's IP addr */
+typedef void (*tcp_connect_cb)(char *,     /* Client's IP addr */
 									 uint32_t);  /* Client's port number */
 
-typedef void (*tcp_conn_killed_cb)(char *,		/*  Client's IP addr */
+typedef void (*tcp_disconnect_cb)(char *,		/*  Client's IP addr */
 								   uint32_t);  	/*  Client's port number */
+
+/* Begin : Working with TCP Connected Clients 
+ * DS to store connected TCP clients connection.
+ * A TCP Connetion is uniquely identified by a
+ * tuple of 4 entries : 
+ * [server ip, server port , client ip, client port]
+ * */
+typedef struct tcp_server_{
+
+	/* key 1 */
+	int master_sock_fd;
+	/* key 2 */
+	char ip_addr[160];
+	uint32_t port_no;
+
+	int dummy_master_sock_fd;
+	recv_fn_cb recv_fn;
+	tcp_disconnect_cb tcp_disconnect_fn;
+	tcp_connect_cb tcp_connect_fn;
+    int monitored_tcp_fd_set_array
+		[MAX_CLIENT_TCP_CONNECTION_SUPPORTED];
+
+	/* 	Other properties below
+		< other tcp server properties >
+	*/
+
+	glthread_t clients_list_head;
+	glthread_t glue;
+} tcp_server_t;
+GLTHREAD_TO_STRUCT(glue_to_tcp_server,
+				   tcp_server_t, glue);
+
+typedef struct tcp_connected_client_{
+
+	/* key 1 */
+	int client_comm_fd;
+	/* key 2 */
+	char client_ip_addr[16];
+	uint32_t client_tcp_port_no;
+	tcp_server_t *tcp_server; /* back pointer */
+	glthread_t glue;
+} tcp_connected_client_t;
+GLTHREAD_TO_STRUCT(glue_to_tcp_connected_client,
+				   tcp_connected_client_t, glue);
+
+typedef struct tcp_connections_{
+
+	glthread_t tcp_server_list_head;
+} tcp_connections_db_t;
+
+void
+tcp_remove_tcp_server_entry(
+	tcp_server_t *tcp_server);
+
+void
+tcp_server_add_to_db(
+	tcp_connections_db_t *tcp_connections_db,
+	tcp_server_t *tcp_server);
+
+void
+tcp_delete_tcp_server_entry(
+	tcp_connections_db_t *tcp_connections_db,
+	tcp_server_t *tcp_server);
+
+tcp_server_t *
+tcp_lookup_tcp_server_entry_by_master_fd(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t master_fd);
+
+void
+tcp_create_new_tcp_connection_client_entry(
+    int client_comm_fd,
+    char *client_ip_addr,
+    uint32_t client_tcp_port_no,
+	tcp_connected_client_t *tcp_connected_client/* o/p */);
+
+tcp_server_t *
+tcp_lookup_tcp_server_entry_by_ipaddr_port(
+	tcp_connections_db_t *tcp_connections_db,
+	char *ip_addr,
+	uint32_t port_no);
+
+tcp_connected_client_t *
+tcp_lookup_tcp_server_client_entry_by_comm_fd(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t comm_fd);
+
+tcp_connected_client_t *
+tcp_lookup_tcp_server_client_entry_by_ipaddr_port(
+	tcp_connections_db_t *tcp_connections_db,
+	char *ip_addr,
+	uint32_t port_no);
+
+void
+tcp_save_tcp_server_client_entry(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t tcp_server_master_sock_fd,
+	tcp_connected_client_t *tcp_connected_client);
+
+void
+tcp_delete_tcp_server_client_entry(
+	tcp_connected_client_t *tcp_connected_client);
+
+void
+tcp_delete_tcp_server_client_entry_by_comm_fd(
+	tcp_connections_db_t *tcp_connections_db,
+	uint32_t comm_fd);
+
+void
+tcp_delete_tcp_server_client_entry_by_ipaddr_port(
+	tcp_connections_db_t *tcp_connections_db,
+	char *ip_addr,
+	uint32_t tcp_port_no);
+
+void
+tcp_dump_tcp_connection_db(
+		tcp_connections_db_t *tcp_connections_db);
+
+/* End : Working with TCP Connected Clients */
 
 void
 network_start_udp_pkt_receiver_thread(
@@ -49,13 +181,20 @@ network_start_tcp_pkt_receiver_thread(
 		char *ip_addr,
 		uint32_t tcp_port_no,
 		recv_fn_cb recv_fn,
-		tcp_conn_init_req_cb conn_init_req_fn,
-		tcp_conn_killed_cb tcp_conn_killed_fn);
+		tcp_connect_cb conn_init_req_fn,
+		tcp_disconnect_cb tcp_conn_killed_fn);
 
 int
 tcp_connect(char *tcp_server_ip,
 			uint32_t tcp_server_port_no);
 
+void
+tcp_fake_connect(char *tcp_server_ip,
+				 uint32_t tcp_server_port_no);
+void
+tcp_disconnect(int comm_sock_fd,
+			   char *good_bye_msg,
+			   uint32_t good_bye_msg_size);
 
 int
 send_udp_msg(char *dest_ip_addr,
@@ -64,8 +203,30 @@ send_udp_msg(char *dest_ip_addr,
 			 uint32_t msg_size);
 
 int
-send_tcp_msg(int tcp_comm_fd,
+tcp_send_msg(int tcp_comm_fd,
 			 char *msg,
 			 uint32_t msg_size);
+
+void
+tcp_force_disconnect_client_by_ip_addr_port(
+		char *ip_addr,
+		uint32_t tcp_port_no);
+
+void
+tcp_force_disconnect_client_by_comm_fd(
+		tcp_connections_db_t *tcp_connections_db,
+		uint32_t comm_fd);
+
+void
+init_network_skt_lib(tcp_connections_db_t 
+	*tcp_connections_db);
+
+/* General Nw utilities */
+char *
+network_covert_ip_n_to_p(uint32_t ip_addr,
+                        char *output_buffer);
+
+uint32_t
+network_covert_ip_p_to_n(char *ip_addr);
 
 #endif /* __NETWORK_UTILS__  */
